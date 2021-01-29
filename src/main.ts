b6688a2363fb4e45b729da08156988f8d6bc58a8
sh1mc <sh1@coins.tsukuba.ac.jp>
2021-01-29 12:54:05 +0900

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,6 @@ const fs = require('fs');
 
 try {
 	const domains = yaml.load(fs.readFileSync('src/data/domains.yml', 'utf8'));
-	console.log((domains as any)[0]['name'])
 
 	const app = express();
 	app.use(helmet());
@@ -16,19 +15,27 @@ try {
 	});
 
 	app.set('view engine', 'pug');
+	app.set('views', 'src/views')
 
 	const secondLevelDomain = /\.?localhost/g;
 	app.get("/", (req, res) => {
 		const domain = req.hostname
 		const subdomain = domain.replace(secondLevelDomain, '');
-		if (true) {
-			res.render('index', {
-				title: 'a',
-				domain: domain,
-				subdomain: subdomain,
-				url: req.originalUrl,
-				img: 'https://shim.earth/shimearth.png'
-			});
+		let exists = false;
+		for (const d of domains) {
+			if(d['name'] === subdomain) {
+				res.render('index', {
+					title: d['title'] + 'します',
+					domain: domain,
+					subdomain: subdomain,
+					url: req.originalUrl,
+					img: 'https://shim.earth/shimearth.png'
+				});
+				exists = true;
+			}
+		}
+		if (!exists) {
+			res.sendStatus(404);
 		}
 	});
 
